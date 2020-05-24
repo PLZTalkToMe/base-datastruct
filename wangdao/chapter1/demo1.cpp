4733ef564992c31838628c37e39b3dee589bbc57
PLZTalkToMe <xxx@xxx.com>
2020-05-24 12:35:51 +0800

    add fibonacci

--- wangdao/chapter1/demo1.cpp
+++ wangdao/chapter1/demo1.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
-int main(){
-    cout << "你好" << endl;
-    return 0;
+
+int fibonacci_rec(int idx) {
+    if (idx == 0) {
+        return 1;
+    } else if (idx == 1) {
+        return 2;
+    } else if(idx < 0){
+        cout << "wrong idx" << endl;
+    }
+    return fibonacci_rec(idx - 1) + fibonacci_rec(idx - 2);
 }
+
+
+
+int fibonacci_loop(int idx) {
+    // todo 循环解决斐波那契问题
+}
+
+
+/**
+ * P12 思维拓展
+ * @return
+ */
+int main() {
+    cout << fibonacci_rec(5) << endl;
+}
+
+
