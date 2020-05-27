06e4ecfb361693ebfb4f0ec90e968d5cc5bc6a37
PLZTalkToMe <xxx@xxx.com>
2020-05-28 00:33:03 +0800

    update

--- wangdao/chapter02/LinkList.cpp
+++ wangdao/chapter02/LinkList.cpp
@@ -3,18 +3,72 @@
 #include <cstdio>
 #include <cstdlib>
 
-
+/**
+ * 头插法
+ * @param L
+ * @return
+ */
 LinkList List_HeadInsert(LinkList &L){
     LNode *s;
     int x;
     L = (LinkList) malloc(sizeof(LNode));
     L -> next = NULL;
-
+    scanf("%d", &x);
+    while(x != 9999){
+        s = (LNode *) malloc(sizeof(LNode));
+        s -> data = x;
+        s ->next = L->next;
+        L ->next = s;
+        scanf("%d", x);
+    }
+    return L;
 }
 
 
+/**
+ * 尾插法
+ * @param L
+ * @return
+ */
+LinkList List_TailInsert(LinkList &L){
+    int x;
+    L = (LinkList) malloc(sizeof(LNode));
+    LNode *s, *r = L;
+    scanf("%d", &x);
+    while(x != 9999){
+        s = (LNode *)malloc(sizeof(LNode));
+        s -> data = x;
+        // 将s加到尾指针后面
+        r -> next = s;
+        // 将r指向新的尾节点
+        r = s;
+    }
+    r -> next = NULL;
+    return L;
+}
 
 
+LNode* GetElem(LinkList L, int i){
+    int j = 1;
+
+    LNode* p = L -> next;
+
+    if(i == 0){
+        return L;
+    }
+    if(i < 0){
+        return NULL;
+    }
+
+    while(p && j < i){
+        p=p->next;
+        j++;
+    }
+
+    return p;
+
+
+}
 
 
 int main(){
